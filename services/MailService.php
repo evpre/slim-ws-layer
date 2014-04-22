df3a7c14dca0f3c9c7f9356a4d4b00c88bcb2d79
evgeny <jonny.predein@apiumtech.com>
2014-04-22 17:55:00 +0200

    wip: inserting alias

--- services/MailService.php
+++ services/MailService.php
@@ -12,7 +12,19 @@ class MailService {
     {
         $domain = $userInfo->domain;
         $userName = $userInfo->userName;
+        $userAlias = $userInfo->userAlias;
+
         $result = shell_exec("sh ../eamida_create_mail_user_SQL.sh $domain $userName");
+
+        if($result){
+            $con=mysqli_connect("localhost","root","root","vmail");
+            if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            }
+            mysqli_query($con,"INSERT INTO alias (address, goto, domain, created, active) VALUES ('$userName', '$userAlias','$domain', NOW(), 1)");
+            mysqli_close($con);
+        }
+
         return $result;
     }
 
